1bfa258c2405b12458407710af7deb4a5a3d49c7
LAG1819 <luisa.gloger@gmx.de>
2025-11-13 12:48:07 +0100

    Format and fix file with linter.

--- tools/Patient.py
+++ tools/Patient.py
@@ -1,60 +1,61 @@
 import json
-from tkinter import END
 
 
 class Patientenliste:
     def __init__(self):
-        self.patientenliste =[]
-        #self.pathk = r'C:\Users\Pepi\AppData\Local\Rechnung_template\kontaktdaten.json'
-        self.pathk= r'kontaktdaten.json'
+        self.patientenliste = []
+        # self.pathk = r'C:\Users\Pepi\AppData\Local\Rechnung_template\kontaktdaten.json'
+        self.pathk = r"kontaktdaten.json"
 
     def addPatient(self, patient):
         self.patientenliste.append(patient)
 
-    def searchPatientByNachname(self,inputPatientenListe,nachname):
-        matches =[]
+    def searchPatientByNachname(self, inputPatientenListe, nachname):
+        matches = []
         for p in inputPatientenListe:
-            nachn= p.getNachname()
+            nachn = p.getNachname()
             if nachname == nachn:
                 matches.append(p)
         return matches
 
-    def searchPatientByVorname(self,inputPatientenListe,vorname):
-        matches =[]
+    def searchPatientByVorname(self, inputPatientenListe, vorname):
+        matches = []
         for p in inputPatientenListe:
-            vorn= p.getVorname()
+            vorn = p.getVorname()
             if vorname == vorn:
                 matches.append(p)
         return matches
 
     def savePatients(self):
-        contacts=[]
+        contacts = []
         tiernamen = []
         for p in self.patientenliste:
-            tiernamen =[]
+            tiernamen = []
             tiernamen.append(p.getTiername())
-            data = {"Anrede":p.getAnrede(),
-                         "Vorname":p.getVorname(),
-                         "Nachname":p.getNachname(),
-                         "Straße":p.getStraße(),
-                         "Addresse": p.getPLZ(),
-                         "Mail": p.getMail(),
-                         "Tierart": p.getTierart(),
-                         "Tiername":tiernamen}
+            data = {
+                "Anrede": p.getAnrede(),
+                "Vorname": p.getVorname(),
+                "Nachname": p.getNachname(),
+                "Straße": p.getStraße(),
+                "Addresse": p.getPLZ(),
+                "Mail": p.getMail(),
+                "Tierart": p.getTierart(),
+                "Tiername": tiernamen,
+            }
             contacts.append(data)
-        #C:\Users\Pepi\AppData\Local\Rechnung_template\
+        # C:\Users\Pepi\AppData\Local\Rechnung_template\
         out_file = open(self.pathk, "w")
-        json.dump(contacts, out_file, indent = 9)
+        json.dump(contacts, out_file, indent=9)
         out_file.close()
 
-    def existPatient(self,person):
+    def existPatient(self, person):
         exist = False
         try:
             personAnrede = person.getAnrede()
             personVorname = person.getVorname()
             personNachname = person.getNachname()
-            #personTier= person.tiername()
-        except:
+            # personTier= person.tiername()
+        except Exception as e:
             exist = True
             return exist
         for p in self.patientenliste:
@@ -63,7 +64,11 @@ class Patientenliste:
             nachname = p.getNachname()
             tiername = p.getTiername()
 
-            if (personAnrede==anrede)and (personVorname==vorname) and (personNachname==nachname):
+            if (
+                (personAnrede == anrede)
+                and (personVorname == vorname)
+                and (personNachname == nachname)
+            ):
                 exist = True
 
         return exist
@@ -105,19 +110,22 @@ class Patientenliste:
     #         patienten=[]
     #         found = False
 
-    def fillout_patient(self,patientenliste,selectedName,selectedBox,nachnameBox):
+    def fillout_patient(self, patientenliste, selectedName, selectedBox, nachnameBox):
         foundPatienten = None
         if nachnameBox:
             nachname = selectedName
-            foundPatienten = self.searchPatientByNachname(patientenliste,nachname)
+            foundPatienten = self.searchPatientByNachname(patientenliste, nachname)
 
         else:
             vorname = selectedName
-            foundPatienten = self.searchPatientByVorname(patientenliste,vorname)
+            foundPatienten = self.searchPatientByVorname(patientenliste, vorname)
         return foundPatienten
 
+
 class Person:
-    def __init__(self,anrede,vorn,nachn,straße_haus, plz_ort, mail,tierart,tiername):
+    def __init__(
+        self, anrede, vorn, nachn, straße_haus, plz_ort, mail, tierart, tiername
+    ):
         self.id = None
         self.anrede = anrede
         self.vorname = vorn
@@ -125,7 +133,7 @@ class Person:
         self.straße_hausnr = straße_haus
         self.addresse = plz_ort
         self.email = mail
-        self.tierart= tierart
+        self.tierart = tierart
         self.tiername = tiername
 
     def getId(self):
@@ -154,4 +162,3 @@ class Person:
 
     def getTiername(self):
         return self.tiername
-
